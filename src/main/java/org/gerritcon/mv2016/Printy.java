b59e5f05981e58219a95017329774020350e8b27
Bruce Zu <bzu@fortinet.com>
2019-06-20 14:09:45 -0700

    Add '--version' but need uber jar
    ```bash
     $ bazel build printy_deploy.jar
    INFO: Analysed target //:printy_deploy.jar (0 packages loaded, 1 target
    configured).
    INFO: Found 1 target...
    Target //:printy_deploy.jar up-to-date:
      bazel-bin/printy_deploy.jar
    INFO: Elapsed time: 0.256s, Critical Path: 0.11s, Remote (0.00% of the
    time): [queue: 0.00%, setup: 0.00%, process: 0.00%]
    INFO: 1 process: 1 linux-sandbox.
    INFO: Build completed successfully, 3 total actions
    
    ```
    
    ```bash
    $ unzip -c -q  bazel-bin/libprinty_lib.jar META-INF/MANIFEST.MF
    Manifest-Version: 1.0
    Created-By: bazel
    Target-Label: //:printy_lib
    
    ```
    
    ```bash
    $ java -jar bazel-bin/printy_deploy.jar --version
    run command line: --version
    1.0
    
    ```

--- src/main/java/org/gerritcon/mv2016/Printy.java
+++ src/main/java/org/gerritcon/mv2016/Printy.java
@@ -1,14 +1,41 @@
 package org.gerritcon.mv2016;
 
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 import com.google.common.base.Joiner;
-public class Printy{
-	public static void main(String [] argv){
-		new Printy().mainImpl(argv);
-	}
-	
-	void mainImpl(String[] argv)   {
+import com.google.common.base.Strings;
 
-		System.out.println(Joiner.on(' ').join(argv));
-	}
+public class Printy {
+  public static void main(String[] argv) throws IOException {
+    System.out.println("run command line: " + Joiner.on(' ').join(argv));
+    if (argv.length > 0 && argv[0].equals("--version")) {
+
+      new Printy().printVersion();
+
+      return;
+    }
+    new Printy().mainImpl(argv);
+  }
+
+  void mainImpl(String[] argv) {
+
+    System.out.println(Joiner.on(' ').join(argv));
+  }
+
+  void printVersion() throws IOException {
+    URLClassLoader cl = (URLClassLoader) getClass().getClassLoader();
+    URL url = cl.findResource("META-INF/MANIFEST.MF");
+    Manifest manifest = new Manifest(url.openStream());
+    Attributes main = manifest.getMainAttributes();
+    String version = main.getValue("Implementation-Version");
+    if (Strings.isNullOrEmpty(version)) {
+      System.err.println("no version specified");
+    } else {
+      System.err.println(version);
+    }
+  }
 }
-
